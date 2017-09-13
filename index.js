2b32344a7a2a2f3d28afb936ac72f9029d53267a
60HRC <31610265+60HRC@users.noreply.github.com>
2017-09-13 10:04:13 +0300

    try try try

--- index.js
+++ index.js
@@ -3,24 +3,24 @@
 var phoneBook = require('./phoneBook');
 
 // Добавляем записи
-phoneBook.add('Сергей', '7 999 6667778', 'gs@example.com');
-phoneBook.add('Сергей 2', '999 4433444', 'gs@example.com');
-phoneBook.add('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
+phoneBook.addContact('Сергей', '7 999 6667778', 'gs@example.com');
+phoneBook.addContact('Сергей 2', '999 4433444', 'gs@example.com');
+phoneBook.addContact('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
 
 // Невалидные данные не должны попадать в книгу!
-phoneBook.add('Честный Хрюндель', 'invalid phone', 'honest-hrundel');
+//phoneBook.add('Честный Хрюндель', 'invalid phone', 'honest-hrundel');
 
-phoneBook.find('777');
+//phoneBook.find('777');
 // Выводит построчно записи, все поля через запятую:
 // Сергей, +7 (999) 666-7-778, gogolef@yandex-team.ru
 // Олег, +7 (999) 777-7-777, just7@yandex-team.ru
 
-phoneBook.remove('Олег');
+//phoneBook.remove('Олег');
 // Выводит количество удалённых контактов, которые удовлетворят запросу:
 // Удален 1 контакт
 
 // Выводит записи в виде красивой таблички
-phoneBook.showTable();
+//phoneBook.showTable();
 // Выводит
 // ┌─────────────┬────────────────────╥──────────────────┐
 // │ Имя         │ Телефон            ║ email            │
@@ -31,5 +31,5 @@ phoneBook.showTable();
 
 
 // Экспортируем записи, пример файла рядом
-phoneBook.importFromCsv('./backup.csv');
+//phoneBook.importFromCsv('./backup.csv');
 // Добавлено 4 контакта
--- phoneBook.js
+++ phoneBook.js
@@ -1,52 +1,12 @@
 'use strict';
 
-var phoneBook; // Здесь вы храните записи как хотите
+var phoneBook = []; // Здесь вы храните записи как хотите
 
-/*
-   Функция добавления записи в телефонную книгу.
-   На вход может прийти что угодно, будьте осторожны.
-*/
-module.exports.add = function add(name, phone, email) {
+module.exports.addContact = function addContact(name, phone, email) {
+    var contact = [];
+    contact.push(name, phone, email);
+    phoneBook.push(contact);
 
-    // Ваша невероятная магия здесь
-
+    return phoneBook;
 };
-
-/*
-   Функция поиска записи в телефонную книгу.
-   Поиск ведется по всем полям.
-*/
-module.exports.find = function find(query) {
-
-    // Ваша удивительная магия здесь
-
-};
-
-/*
-   Функция удаления записи в телефонной книге.
-*/
-module.exports.remove = function remove(query) {
-
-    // Ваша необьяснимая магия здесь
-
-};
-
-/*
-   Функция импорта записей из файла (задача со звёздочкой!).
-*/
-module.exports.importFromCsv = function importFromCsv(filename) {
-    var data = require('fs').readFileSync(filename, 'utf-8');
-
-    // Ваша чёрная магия:
-    // - Разбираете записи из `data`
-    // - Добавляете каждую запись в книгу
-};
-
-/*
-   Функция вывода всех телефонов в виде ASCII (задача со звёздочкой!).
-*/
-module.exports.showTable = function showTable() {
-
-    // Ваша чёрная магия здесь
-
-};
+module.exports.addContact();
