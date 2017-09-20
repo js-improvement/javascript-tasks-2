92b33d5438a3d34d3cf497fa4618ccaeb4ec810f
60HRC <31610265+60HRC@users.noreply.github.com>
2017-09-20 12:36:10 +0300

    over 100500 строк кода без regep и csv

--- index.js
+++ index.js
@@ -5,22 +5,20 @@ var phoneBook = require('./phoneBook');
 // Добавляем записи
 phoneBook.addContact('Сергей', '7 999 6667778', 'gs@example.com');
 phoneBook.addContact('Сергей 2', '999 4433444', 'gs@example.com');
-phoneBook.addContact('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
+phoneBook.addContact('MrTrump', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
+//console.info(phoneBook.addContact('Честный Хрюндель', 'invalid phone', 'honest-hrundel'));
 
-// Невалидные данные не должны попадать в книгу!
-//phoneBook.add('Честный Хрюндель', 'invalid phone', 'honest-hrundel');
-
-//phoneBook.find('777');
+phoneBook.find('3');
 // Выводит построчно записи, все поля через запятую:
 // Сергей, +7 (999) 666-7-778, gogolef@yandex-team.ru
 // Олег, +7 (999) 777-7-777, just7@yandex-team.ru
 
-//phoneBook.remove('Олег');
+phoneBook.remove('3');
 // Выводит количество удалённых контактов, которые удовлетворят запросу:
 // Удален 1 контакт
 
 // Выводит записи в виде красивой таблички
-//phoneBook.showTable();
+phoneBook.showTable();
 // Выводит
 // ┌─────────────┬────────────────────╥──────────────────┐
 // │ Имя         │ Телефон            ║ email            │
@@ -31,5 +29,5 @@ phoneBook.addContact('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
 
 
 // Экспортируем записи, пример файла рядом
-//phoneBook.importFromCsv('./backup.csv');
+// phoneBook.importFromCsv('./backup.csv');
 // Добавлено 4 контакта
--- phoneBook.js
+++ phoneBook.js
@@ -1,7 +1,90 @@
 'use strict';
 
-var phoneBook = []; // Здесь вы храните записи как хотите
+var phoneBook = [];
 
+// function validContact(name, phone, email) {
+// }
+/**
+ * ФУНКЦИЯ ИЩЕТ МАКСИМАЛЬНУЮ ДЛИННУ ЭЛЕМЕНТА
+ * ДЛЯ ПОСЛЕДУЮЩЕГО ВЫВОДА В ВИДЕ ТАБЛИЦЫ
+ * С ИСПОЛЬЗОВАНИЕМ СИМВОЛОВ ASCII
+ * @param numbOfColumn -НОМЕР КОЛОНКИ
+ * 0 -КОЛОНКА ИМЕН
+ * 1 -КОЛОНКА ТЕЛЕФОННЫХ НОМЕРОВ
+ * 2 -КОЛОНКА ЭЛЕКТРОННОЙ ПОЧТЫ
+ * @returns {number} -ВОЗВРАЩАЕТ МАКСИМАЛЬНУЮ ДЛИННУ
+ */
+function columnMaxLenght(numbOfColumn) {
+    var maxLenght = 0;
+    for (var i = 0; i < phoneBook.length; i++) {
+        if (phoneBook[i][numbOfColumn].length > maxLenght) {
+            maxLenght = phoneBook[i][numbOfColumn].length;
+        }
+    }
+
+    return maxLenght;
+}
+
+/**
+ * МЕГА ФУНКЦИЯ ПОИСКА
+ * КОТОРАЯ БУДЕТ ИСПОЛЬЗОВАНА В ДАЛЬНЕЙШЕМ
+ * ДЛЯ ПОИСКА И УДАЛЕНИЯ КОНТАКТОВ
+ * @param inputData -ШАБЛОН ПОИСКА В ВИДЕ СТРОКИ
+ * @param phoneBook -ТЕЛЕФОННАЯ КНИГА
+ * @returns {Array} -МАСИИВ НАЙДЕННЫХ КОНТАКТОВ
+ */
+function finder(inputData, phoneBook) {
+    var searchPattern = new RegExp(inputData, 'i');
+    var findedContacts = [];
+    if (inputData === undefined || inputData === null) {
+
+        return phoneBook;
+    }
+    for (var i = 0; i < phoneBook.length; i++) {
+        if (searchPattern.test(phoneBook[i].toString()) === true) {
+            findedContacts.push(phoneBook[i]);
+        }
+    }
+    if (findedContacts.length === 0) {
+
+        return false;
+    }
+
+    return findedContacts;
+
+}
+
+/**
+ * ПОЛНЫЙ ТРЭШ!!!
+ * НАПИСАНО В СООАВТОРСТВЕ С САТАНОЙ
+ * ГЛУБОКОЙ НОЧЬЮ ПОД ДВУМЯ ДОЗАМИ ДВОЙНОГО ЭСПРЕССО
+ * ПЕЧАТАЕМ ГОВНОТАБЛИЧКУ
+ * @param arrayToPrint -МАСИИВ ДАННЫХ, ПОСТУПАЮЩИЙ НА ПЕЧАТЬ
+ */
+function printTable(arrayToPrint) {
+    console.info('-'.repeat(columnMaxLenght(0) + columnMaxLenght(1) + columnMaxLenght(2) + 10));
+    console.info('| ИМЯ' + ' '.repeat(columnMaxLenght(0) - 2) + '| ТЕЛЕФОН' +
+    ' '.repeat(columnMaxLenght(1) - 6) + '| eMAIL' + ' '.repeat(columnMaxLenght(2)-4) + '|');
+    console.info('-'.repeat(columnMaxLenght(0) + columnMaxLenght(1) + columnMaxLenght(2) + 10));
+    //console.info(arrayToPrint);
+    for (var i = 0; i < arrayToPrint.length; i++) {
+        console.info('| ' + arrayToPrint[i][0] + ' '.repeat(columnMaxLenght(0) - arrayToPrint[i][0].length + 1) +
+            '| ' + arrayToPrint[i][1] + ' '.repeat(columnMaxLenght(1) - arrayToPrint[i][1].length + 1) +
+            '| ' + arrayToPrint[i][2] + ' '.repeat(columnMaxLenght(2) - arrayToPrint[i][2].length + 1) + '|');
+    }
+    console.info('-'.repeat(columnMaxLenght(0) + columnMaxLenght(1) + columnMaxLenght(2) + 10) + '\n');
+
+}
+
+/**
+ * ФУНКЦИЯ ДОБАВЛЕНИЯ ЗАПИСИ В ТЕЛЕФОННУЮ КНИГУ
+ * В ВИДЕ МАССИВА
+ * ЕСЛИ ДАННЫЕ ПРОХОДЯТ ПРОВЕРКУ НА ВАЛИДНОСТЬ
+ * @param name - ИМЯ КОНТАКТА
+ * @param phone - НОМЕР ТЕЛЕФОНА
+ * @param email - ЭЛЕКТРОННАЯ ПОЧТА
+ * @returns {Array} - НА ВЫХОДЕ МАССИВ ИЗ КОНТАКТОВ
+ **/
 module.exports.addContact = function addContact(name, phone, email) {
     var contact = [];
     contact.push(name, phone, email);
@@ -9,4 +92,57 @@ module.exports.addContact = function addContact(name, phone, email) {
 
     return phoneBook;
 };
-module.exports.addContact();
+
+/**
+ * ФУНКЦИЯ ПОИСКА
+ * ПЕЧАТАЕТ ТАБЛИЧКУ С МАССИВОМ
+ * РЕЗУЛЬТАТОВ ПОИСКА
+ * И КОЛИЧЕСТВО СОВПАДЕНИЙ
+ * ИЛИ РУГАТЕЛЬСТВО
+ * @param query -ПРИНИМАЕТ СТРОКОВОЕ ЗНАЧЕНИЕ ДЛЯ ПОИСКА
+ */
+module.exports.find = function find(query) {
+    if (finder(query, phoneBook) === false) {
+        console.info(query + '  <-----------<  I CAN NOT FIND THIS SHIT!\n');
+    } else {
+        console.info('НАЙДЕНО КОНТАКТОВ: ' + finder(query, phoneBook).length);
+        printTable(finder(query, phoneBook));
+    }
+};
+
+/**
+ * ФУНКЦИЯ УДАЛЕНИЯ КОНТАКТА
+ * ПЕЧАТАЕТ ТАБЛИЧКУ УДАЛЕННЫХ КОНТАКТОВ
+ * И ИХ КОЛИЧЕСТВО
+ * ИЛИ РУГАТЕЛЬСТВО
+ * @param query -ПРИНИМАЕТ СТРОКОВОЕ ЗНАЧЕНИЕ ДЛЯ ПОИСКА
+ * УДАЛЯЕМЫХ КОНТАКТОВ
+ */
+module.exports.remove = function remove(query) {
+    var findedArr = finder(query, phoneBook);
+    if (finder(query, phoneBook) === false) {
+        console.info(query + '  <-----------<  I CAN NOT FIND AND DELETE THIS SHIT!\n');
+    } else {
+        let newBook = phoneBook.filter( x => findedArr.indexOf(x) === -1);//КАК ЗАВЕЩАЛ ЕВСТРОПОВ
+        //СТЫРИЛ С STACKOVERFLOW.COM
+        phoneBook = newBook;
+        console.info('УДАЛЕНО КОНТАКТОВ: ' + findedArr.length);
+        printTable(findedArr);
+    }
+};
+
+module.exports.importFromCsv = function importFromCsv(filename) {
+    var data = require('fs').readFileSync(filename, 'utf-8');
+};
+
+/**
+ * ФУНКЦИЯ ПЕЧАТИ СОДЕРЖИМОГО ТЕЛЕФОННОЙ КНИГИ
+ * ПЕЧАТАЕТ ТАБЛИЧКУ С ТЕЛЕФОННОЙ КНИГОЙ
+ * БЕЗ УДАЛЕННЫХ КОНТАКТОВ
+ * ЕСЛИ, КОНЕЧНО, ЧТО ТО БЫЛО УДАЛЕНО
+ */
+module.exports.showTable = function showTable() {
+    console.info('ПОСЛЕДНЯЯ ВЕРСИЯ ТЕЛЕФОННОЙ КНИГИ :');
+    printTable(phoneBook);
+};
+
