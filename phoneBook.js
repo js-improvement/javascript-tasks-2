ceb2de645147cb8ee0686ee5515cfe202e0d898a
ISKondratyev <kotoezhikne@gmail.com>
2018-01-29 12:18:20 +0300

    Добавлен вывод в таблицу без учета длины максимальной записи. + Добавление записей без фильтров.

--- phoneBook.js
+++ phoneBook.js
@@ -1,15 +1,24 @@
 'use strict';
 
-var phoneBook; // Здесь вы храните записи как хотите
+var phoneBook = []; // Здесь вы храните записи как хотите
 
 /*
    Функция добавления записи в телефонную книгу.
    На вход может прийти что угодно, будьте осторожны.
 */
 module.exports.add = function add(name, phone, email) {
-
     // Ваша невероятная магия здесь
+    if ((name === '') || (phone === '') || (email === '')) {
+        return;
+    }
 
+    var person = {
+        name: name,
+        phone: phone,
+        email: email
+    };
+
+    phoneBook.push(person);
 };
 
 /*
@@ -46,7 +55,55 @@ module.exports.importFromCsv = function importFromCsv(filename) {
    Функция вывода всех телефонов в виде ASCII (задача со звёздочкой!).
 */
 module.exports.showTable = function showTable() {
+    tableTop();
 
-    // Ваша чёрная магия здесь
+    for (var i = 0; i < phoneBook.length; i++) {
+        var line = symb(9474);
+        line = line + ' ' + phoneBook[i].name + spaces(12, phoneBook[i].name.length) + symb(9474);
+        line = line + ' ' + phoneBook[i].phone + spaces(19, phoneBook[i].phone.length) + symb(9553);
+        line = line + ' ' + phoneBook[i].email + spaces(17, phoneBook[i].email.length) + symb(9474);
+        console.log(line);
+    }
 
+    tableBottom();
 };
+
+// phoneBook.add('Сергей', '7 999 6667778', 'gs@example.com');
+// phoneBook.add('Сергей', '7 999 6667778', 'gs@example.com');
+// phoneBook.add('Сергей 2', '999 4433444', 'gs@example.com');
+// phoneBook.add('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
+// phoneBook.push(['Name', 'tel', 'email']);
+// phoneBook.push(['Name2', 'tel2', 'email2']);
+
+function HLine(width) {
+    var line = '';
+    for (var i = 0; i < width; i++) {
+        line = line + symb(9472);
+    }
+
+    return line;
+}
+
+function symb(codeValue) {
+    return String.fromCharCode(codeValue);
+}
+
+function tableTop() {
+    console.log(symb(9484) + HLine(13) + symb(9516) + HLine(20) + symb(9573) + HLine(18) + symb(9488));
+    console.log(symb(9474) + ' Имя' + spaces(12, 3) + symb(9474) + ' Телефон' + spaces(19, 7) + symb(9553) + ' email' + spaces(17, 5) + symb(9474));
+    console.log(symb(9500) + HLine(13) + symb(9532) + HLine(20) + symb(9579) + HLine(18) + symb(9508));
+}
+
+function tableBottom() {
+    console.log(symb(9492) + HLine(13) + symb(9524) + HLine(20) + symb(9576) + HLine(18) + symb(9496));
+}
+
+function spaces(maxSpacesAmount, reduceAmount) {
+    var spacesAmount = maxSpacesAmount - reduceAmount;
+    var line = '';
+    for (var i = 0; i < spacesAmount; i++) {
+        line = line + ' ';
+    }
+
+    return line;
+}
